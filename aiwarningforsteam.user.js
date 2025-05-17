04f87ee7b3e9c260cabb5674ab99fa6600df2bc9
Sewan Aleanakian <seeeeew@users.noreply.github.com>
2025-05-18 01:14:56 +0200

    added translation for all languages officially supported by the Steam store

--- aiwarningforsteam.user.js
+++ aiwarningforsteam.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AI warning popup for Steam
 // @namespace    https://github.com/seeeeew/aiwarningforsteam/
-// @version      0.1.0
+// @version      0.2.0
 // @description  Shows the AI generated content disclosure on Steam store pages as a modal popup.
 // @author       seeeeew
 // @match        https://store.steampowered.com/app/*
@@ -12,43 +12,84 @@
 
 (function() {
 
-	const strings = {
-		schinese: "AI 生成内容披露",
-		tchinese: "AI 生成內容聲明",
-		japanese: "AI生成コンテンツの開示",
-		koreana: "AI 생성 콘텐츠 사용 공개",
-		thai: "การเปิดเผยข้อมูลเกี่ยวกับเนื้อหาที่สร้างด้วย AI",
-		bulgarian: "Оповестяване за съдържание, генерирано от ИИ",
-		czech: "Informace o obsahu vytvářeném AI",
-		danish: "Meddelelse om AI-genereret indhold",
-		german: "Offenlegung von KI-generierten Inhalten",
-		english: "AI Generated Content Disclosure",
-		spanish: "Información sobre contenido generado por IA",
-		latam: "Información sobre contenido generado por IA",
-		greek: "Γνωστοποίηση περιεχομένου που δημιουργήθηκε από τεχνητή νοημοσύνη (AI)",
-		french: "Divulgation de contenu généré par IA",
-		italian: "Divulgazione dei contenuti generati dall'IA",
-		indonesian: "Pernyataan Konten Buatan AI",
-		hungarian: "Nyilatkozat MI generálta tartalomról",
-		dutch: "Informatie over door AI gegenereerde inhoud",
-		norwegian: "Opplysning om AI-generert innhold",
-		polish: "Oświadczenie w sprawie treści generowanych przez SI",
-		portuguese: "Divulgação de conteúdo gerado por IA",
-		brazilian: "Divulgação de conteúdo gerado por IA",
-		romanian: "Informații despre conținutul generat de IA",
-		russian: "Информация о контенте, созданном с помощью ИИ",
-		finnish: "Tiedote tekoälysisällöstä",
-		swedish: "Upplysning om AI-genererat innehåll",
-		turkish: "Yapay Zekâ İçeriği Açıklaması",
-		vietnamese: "Công bố về nội dung tạo bởi AI",
-		ukrainian: "Розкриття інформації щодо вмісту, згенерованого ШІ",
-	};
+	const msg = {
+		aidisclosure: {
+			schinese: "AI 生成内容披露",
+			tchinese: "AI 生成內容聲明",
+			japanese: "AI生成コンテンツの開示",
+			koreana: "AI 생성 콘텐츠 사용 공개",
+			thai: "การเปิดเผยข้อมูลเกี่ยวกับเนื้อหาที่สร้างด้วย AI",
+			bulgarian: "Оповестяване за съдържание, генерирано от ИИ",
+			czech: "Informace o obsahu vytvářeném AI",
+			danish: "Meddelelse om AI-genereret indhold",
+			german: "Offenlegung von KI-generierten Inhalten",
+			english: "AI Generated Content Disclosure",
+			spanish: "Información sobre contenido generado por IA",
+			latam: "Información sobre contenido generado por IA",
+			greek: "Γνωστοποίηση περιεχομένου που δημιουργήθηκε από τεχνητή νοημοσύνη (AI)",
+			french: "Divulgation de contenu généré par IA",
+			italian: "Divulgazione dei contenuti generati dall'IA",
+			indonesian: "Pernyataan Konten Buatan AI",
+			hungarian: "Nyilatkozat MI generálta tartalomról",
+			dutch: "Informatie over door AI gegenereerde inhoud",
+			norwegian: "Opplysning om AI-generert innhold",
+			polish: "Oświadczenie w sprawie treści generowanych przez SI",
+			portuguese: "Divulgação de conteúdo gerado por IA",
+			brazilian: "Divulgação de conteúdo gerado por IA",
+			romanian: "Informații despre conținutul generat de IA",
+			russian: "Информация о контенте, созданном с помощью ИИ",
+			finnish: "Tiedote tekoälysisällöstä",
+			swedish: "Upplysning om AI-genererat innehåll",
+			turkish: "Yapay Zekâ İçeriği Açıklaması",
+			vietnamese: "Công bố về nội dung tạo bởi AI",
+			ukrainian: "Розкриття інформації щодо вмісту, згенерованого ШІ",
+		},
+		close: {
+			schinese: "关闭",
+			tchinese: "關閉",
+			japanese: "閉じる",
+			koreana: "닫기",
+			thai: "ปิด",
+			bulgarian: "Затваряне",
+			czech: "Zavřít",
+			danish: "Luk",
+			german: "Schließen",
+			english: "Close",
+			spanish: "Cerrar",
+			latam: "Cerrar",
+			greek: "Κλείσιμο",
+			french: "Fermer",
+			italian: "Chiudi",
+			indonesian: "Tutup",
+			hungarian: "Bezárás",
+			dutch: "Sluiten",
+			norwegian: "Lukk",
+			polish: "Zamknij",
+			portuguese: "Fechar",
+			brazilian: "Fechar",
+			romanian: "Închide",
+			russian: "Закрыть",
+			finnish: "Sulje",
+			swedish: "Stäng",
+			turkish: "Kapat",
+			vietnamese: "Đóng",
+			ukrainian: "Закрити",
+		}
+		// Currently latam and brazilian aren't recognized and are only included
+		// for completeness, but we can ignore that for now, because both their
+		// strings are identical to spanish and portuguese respectively.
+	}
 
 	function findAIDisclosureHeader() {
-		return [...document.querySelectorAll(".game_page_autocollapse > #game_area_content_descriptors > h2")].filter(element => Object.values(strings).includes(element.textContent))[0];
+		return [...document.querySelectorAll(".game_page_autocollapse > #game_area_content_descriptors > h2")].filter(element => Object.values(msg.aidisclosure).includes(element.textContent))[0];
+	}
+
+	function findKeyForValue(object, value) {
+		return Object.keys(object).find(key => object[key] === value);
 	}
 
 	function createWarning(header) {
+		const language = findKeyForValue(msg.aidisclosure, header.textContent);
 		const container = document.createElement("div");
 		container.classList.add("aiwarning_container");
 		container.style.position = "fixed";
@@ -73,7 +114,7 @@
 						<div class="newmodal_prompt_description fullwidth aiwarning_content"></div>
 						<div style="float: right">
 							<div class="btn_blue_steamui btn_medium aiwarning_close">
-								<span>Close</span>
+								<span>${msg.close[language]}</span>
 							</div>
 						</div>
 					</div>
@@ -81,7 +122,7 @@
 			</div>
 		`;
 		container.querySelector(".aiwarning_header").textContent = header.textContent;
-		container.querySelector(".aiwarning_content").append(...[...header.parentNode.cloneNode(true).childNodes].filter(node => node.tagName != "H2"));
+		container.querySelector(".aiwarning_content").append(...[...header.parentNode.cloneNode(true).childNodes].filter(node => node.tagName !== "H2"));
 		container.querySelectorAll(".aiwarning_close").forEach(element => element.addEventListener("click", event => container.remove()));
 		container.addEventListener("click", event => {
 			if (event.target === container) container.remove();
