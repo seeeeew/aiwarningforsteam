af1cadba72ebe2182348195279133a3aa470ff53
seeeeew <seeeeew@users.noreply.github.com>
2026-06-20 01:01:12 +0200

    Added search filter by AI disclosure

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # AI warning for Steam
 
-Shows the _AI Generated Content Disclosure_ on Steam store pages as a modal popup.
+**AI warning for Steam** is a browser extension and userscript that adds features to increase the visibility of AI Generated Content Disclosures in the Steam store.
 
-![Screenshot of a popup showing an AI Generated Content Disclosure in the Steam store](img/screenshot_github.png)
+![Screenshot of a popup showing an AI Generated Content Disclosure in the Steam store](img/screenshot_github_popup.png)
 
-If a Steam store page contains an _AI Generated Content Disclosure_, this extension will display it as a modal popup, instead of just at the end of the page. Additionally, it will blur out the rest of the page until the popup is dismissed.
+![Screenshot of a several search results in the Steam store, some of which are blurred and marked as having an AI Generated Content Disclosure](img/screenshot_github_search.png)
+
+## Features
+
+- Display the AI Generated Content Disclosure on Steam store pages as a model popup on page load, if present.
+- Blur or hide games with an AI Generated Content Disclosure on search pages in the Steam store.
 
 ## Installation
 
@@ -19,17 +24,17 @@ This is the easier option, but updates can sometimes be delayed, because Mozilla
 ### Option 2: Install as a userscript
 
 This option is slightly less straight-forward, if you've never used a userscript, but you get updates as soon as they're available.
-1. If necessary, install a userscript manager for your browser, for example by following the instructions at [Greasy Fork](https://greasyfork.org/#home-step-1).
+1. If necessary, install a userscript manager for your browser. ([Greasy Fork](https://greasyfork.org/#home-step-1) has instructions.)
 2. [Click here to install the userscript](https://github.com/seeeeew/aiwarningforsteam/raw/refs/heads/main/aiwarningforsteam.user.js) _or_ download [`aiwarningforsteam.user.js`](https://github.com/seeeeew/aiwarningforsteam/blob/main/aiwarningforsteam.user.js) to install it manually.
 
-## Platform and Language Support
+## Platform and language support
 
 The native browser extension should work in any up-to-date version of Firefox, Chrome, or any related browsers that can install extensions from the same sources, on any desktop OS. The Firefox version also works on mobile systems.
 
 The userscript should work in any up-to-date browser with userscript support on any desktop or mobile OS.
 
-The extension supports all languages officially supported by the Steam store, but the download/installation pages as well as metadata like the extension description are only available in English.
+The extension should work in all languages officially supported by the Steam store, but not all features are translated into languages other than English.
 
-## Project Scope
+## Project goal
 
-This extension's sole purpose is making the _AI Generated Content Disclosure_ impossible to miss on Steam store pages where it exists. Future updates might improve this functionality, but will not add unrelated features.
+The purpose of this extension is to give users better visibility of information about whether generative AI was used in the creation of games they're browsing on Steam. Future updates might improve this functionality, but will not add unrelated features.
--- aiwarningforsteam.user.js
+++ aiwarningforsteam.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         AI warning for Steam
 // @namespace    https://github.com/seeeeew/aiwarningforsteam
-// @version      1.0.3
-// @description  Shows the AI Generated Content Disclosure on Steam store pages as a modal popup.
+// @version      1.1.0
+// @description  Adds features to increase the visibility of AI Generated Content Disclosures.
 // @author       seeeeew
 // @homepage     https://github.com/seeeeew/aiwarningforsteam
 // @match        https://store.steampowered.com/app/*
+// @match        https://store.steampowered.com/search*
 // @icon         https://raw.githubusercontent.com/seeeeew/aiwarningforsteam/refs/heads/main/img/icon128.png
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @run-at       document-end
 // @updateURL    https://github.com/seeeeew/aiwarningforsteam/raw/refs/heads/main/aiwarningforsteam.user.js
 // @downloadURL  https://github.com/seeeeew/aiwarningforsteam/raw/refs/heads/main/aiwarningforsteam.user.js
@@ -16,6 +18,8 @@
 // ==/UserScript==
 
 (function() {
+
+	if (window.browser == undefined && window.chrome !== undefined) window.browser = window.chrome;
 
 	const msg = {
 		aidisclosure: {
@@ -87,14 +91,89 @@
 		// strings are identical to spanish and portuguese respectively.
 	}
 
-	function findAIDisclosureHeader() {
-		const titles = Object.values(msg.aidisclosure);
-		return [...document.querySelectorAll(".game_page_autocollapse > #game_area_content_descriptors > h2")].find(element => titles.includes(element.textContent));
+	function getMetadata() {
+		const metadata = {};
+		if (typeof GM_info !== "undefined") {
+			metadata.homepage = GM_info.script.homepage;
+			metadata.name = GM_info.script.name;
+			metadata.version = GM_info.script.version;
+		} else if (typeof browser !== "undefined") {
+			metadata.homepage = browser.runtime.getManifest().homepage_url;
+			metadata.name = browser.runtime.getManifest().name;
+			metadata.version = browser.runtime.getManifest().version;
+		}
+		return metadata;
+	}
+	const {homepage, name, version} = getMetadata();
+
+	const Config = {
+		set: async (key, value) => {
+			if (typeof GM_setValue !== "undefined") {
+				GM_setValue(key, value);
+			} else if (typeof browser !== "undefined") {
+				await browser.storage.sync.set({[key]: value});
+			}
+		},
+		get: async (key) => {
+			if (typeof GM_getValue !== "undefined") {
+				return GM_getValue(key);
+			} else if (typeof browser !== "undefined") {
+				return (await browser.storage.sync.get(key))[key];
+			}
+		}
 	}
 
-	function injectStyle() {
+	const AIDisclosureCache = {
+		set: async (appid, aidisclosure) => {
+			const key = "app-" + appid;
+			const value = {
+				aidisclosure,
+				updated: new Date().toISOString()
+			};
+			if (typeof GM_setValue !== "undefined") {
+				GM_setValue(key, value);
+			} else if (typeof browser !== "undefined") {
+				await browser.storage.local.set({[key]: value});
+			}
+		},
+		get: async (appid) => {
+			const key = "app-" + appid;
+			let value;
+			if (typeof GM_getValue !== "undefined") {
+				value = GM_getValue(key);
+			} else if (typeof browser !== "undefined") {
+				value = (await browser.storage.local.get(key))[key];
+			}
+			const {aidisclosure, updated: updatedString} = value || {};
+			const age = Date.now() - new Date(updatedString).getTime();
+			if (age < (aidisclosure ? 30 : 7) * 24 * 60 * 60 * 1000) {
+				return aidisclosure;
+			}
+		}
+	};
+
+	function html(strings, ...values) {
+		const template = document.createElement("template");
+		template.innerHTML = String.raw({raw: strings}, ...values).trim();
+		const nodes = template.content.childNodes;
+		return nodes.length === 1 ? nodes[0] : nodes;
+	}
+
+	function injectStyle(source) {
 		const style = document.createElement("style");
-		style.innerHTML = `
+		style.innerHTML = source;
+		style.id = "aiwarning_style";
+		document.head.append(style);
+		return style;
+	}
+
+	if (window.location.pathname.match(/^\/app(\/|$)/)) {
+		function findAIDisclosureHeader() {
+			const titles = Object.values(msg.aidisclosure);
+			return [...document.querySelectorAll(".game_page_autocollapse > #game_area_content_descriptors > h2")].find(element => titles.includes(element.textContent));
+		}
+
+		const css = `
 			.aiwarning_container {
 				position: fixed;
 				inset: 0px;
@@ -128,78 +207,227 @@
 				opacity: 0.6;
 			}
 		`;
-		document.head.append(style);
-		return style;
-	}
 
-	function getMetadata() {
-		const metadata = {};
-		if (typeof GM_info !== "undefined") {
-			metadata.homepage = GM_info.script.homepage;
-			metadata.name = GM_info.script.name;
-			metadata.version = GM_info.script.version;
-		} else if (typeof browser !== "undefined") {
-			metadata.homepage = browser.runtime.getManifest().homepage_url;
-			metadata.name = browser.runtime.getManifest().name;
-			metadata.version = browser.runtime.getManifest().version;
-		} else if (typeof chrome !== "undefined") {
-			metadata.homepage = chrome.runtime.getManifest().homepage_url;
-			metadata.name = chrome.runtime.getManifest().name;
-			metadata.version = chrome.runtime.getManifest().version;
+		function findKeyForValue(object, value) {
+			return Object.keys(object).find(key => object[key] === value);
 		}
-		return metadata;
-	}
 
-	function findKeyForValue(object, value) {
-		return Object.keys(object).find(key => object[key] === value);
-	}
-
-	function createWarning(header) {
-		const style = injectStyle();
-		const {homepage, name, version} = getMetadata();
-		const language = findKeyForValue(msg.aidisclosure, header.textContent);
-		const container = document.createElement("div");
-		container.classList.add("aiwarning_container");
-		const appname = document.querySelector("#appHubAppName")?.textContent;
-		const title = header.innerHTML + (appname ? " — " + appname : "");
-		container.innerHTML = `
-			<div class="newmodal">
-				<div class="modal_top_bar"></div>
-				<div class="newmodal_header_border">
-					<div class="newmodal_header">
-						<div class="newmodal_close"></div>
-						<div class="title_text">${title}</div>
-					</div>
-				</div>
-				<div class="newmodal_content_border">
-					<div class="newmodal_content">
-						<div class="newmodal_prompt_description"></div>
-						<div class="newmodal_buttons">
-							<div class="btn_blue_steamui btn_medium aiwarning_close">
-								<span>${msg.close[language]}</span>
+		function createWarning(header) {
+			const style = injectStyle(css);
+			const language = findKeyForValue(msg.aidisclosure, header.textContent);
+			const appname = document.querySelector("#appHubAppName")?.textContent;
+			const title = header.innerHTML + (appname ? " — " + appname : "");
+			const container = html`
+				<div class="aiwarning_container">
+					<div class="newmodal">
+						<div class="modal_top_bar"></div>
+						<div class="newmodal_header_border">
+							<div class="newmodal_header">
+								<div class="newmodal_close"></div>
+								<div class="title_text">${title}</div>
 							</div>
 						</div>
+						<div class="newmodal_content_border">
+							<div class="newmodal_content">
+								<div class="newmodal_prompt_description"></div>
+								<div class="newmodal_buttons">
+									<div class="btn_blue_steamui btn_medium aiwarning_close">
+										<span>${msg.close[language]}</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<a href="${homepage}" class="aiwarning_watermark">${name} v${version}</a>
 					</div>
 				</div>
-				<a href="${homepage}" class="aiwarning_watermark">${name} v${version}</a>
-			</div>
-		`;
-		container.querySelector(".newmodal_prompt_description").append(...[...header.parentNode.childNodes].filter(node => node !== header).map(node => node.cloneNode(true)));
-		function closeWarning() {
-			container.remove();
-			style.remove();
+			`;
+			container.querySelector(".newmodal_prompt_description").append(...[...header.parentNode.childNodes].filter(node => node !== header).map(node => node.cloneNode(true)));
+			function closeWarning() {
+				container.remove();
+				style.remove();
+			}
+			container.querySelectorAll(".newmodal_close, .aiwarning_close").forEach(element => element.addEventListener("click", closeWarning));
+			container.addEventListener("click", event => {
+				if (event.target === container) closeWarning();
+			});
+			document.body.append(container);
 		}
-		container.querySelectorAll(".newmodal_close, .aiwarning_close").forEach(element => element.addEventListener("click", closeWarning));
-		container.addEventListener("click", event => {
-			if (event.target === container) closeWarning();
-		});
-		document.body.append(container);
+
+		const header = findAIDisclosureHeader();
+		const warning = document.querySelector(".aiwarning_container");
+		if (header && !warning) createWarning(header);
+		const appid = window.location.pathname.match("^/app/(?<appid>\\d+)")?.groups.appid;
+		if (appid) AIDisclosureCache.set(appid, !!header);
 	}
 
-	const header = findAIDisclosureHeader();
-	const warning = document.querySelector(".aiwarning_container");
-	if (header && !warning) {
-		createWarning(header);
-	}
+	if (window.location.pathname.match(/^\/search(\/|$)/)) {
+		const css = `
+			#search_resultsRows > a.search_result_row.aiwarning_checking .responsive_search_name_combined::after {
+				position: absolute;
+				top: 0;
+				right: 0;
+				content: "Checking for ${msg.aidisclosure.english}";
+				background: url("https://community.fastly.steamstatic.com/public/images/login/throbber.gif") no-repeat;
+				background-size: 1lh;
+				height: 1lh;
+				font-size: 10px;
+				padding-left: 1.25lh;
+				margin: 2px 4px;
+			}
+			[data-aiwarning]:not([data-aiwarning=show]) #search_resultsRows > a.search_result_row.aiwarning_failed .responsive_search_name_combined::after {
+				position: absolute;
+				top: 0;
+				right: 0;
+				content: "Checking for ${msg.aidisclosure.english} failed";
+				font-size: 10px;
+				margin: 2px 4px;
+				color: red;
+				opacity: 0.5;
+			}
+			[data-aiwarning=blur] #search_resultsRows > a.search_result_row.aiwarning_hasdisclosure:not(:hover) {
+				&::before {
+					z-index: 998;
+					position: absolute;
+					inset: 0 0 0 0;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					content: "${msg.aidisclosure.english} found";
+					font-size: clamp(12px, 1.3vw, 16px);
+					font-weight: bold;
+				}
+				&::after {
+					z-index: 998;
+					position: absolute;
+					inset: 0 0 0 0;
+					display: flex;
+					justify-content: center;
+					align-items: flex-end;
+					margin: 2px 4px;
+					content: "${name} v${version}";
+					font-size: 10px;
+					color: white;
+					opacity: 0.25;
+				}
+				& > * {
+					filter: blur(10px);
+				}
+			}
+			[data-aiwarning=hide] #search_resultsRows > a.search_result_row.aiwarning_hasdisclosure {
+				display: none;
+			}
+			#aiwarning_filter .block_content {
+				transition: 200ms ease-in-out;
+				transition-property: max-height, padding, margin;
+				overflow-y: hidden;
+			}
+			#aiwarning_filter.collapsed .block_content {
+				padding-top: 0;
+				padding-bottom: 0;
+				margin-top: 0;
+				margin-bottom: 0;
+			}
+		`;
+		injectStyle(css);
 
+		const search_results = document.getElementById("search_results");
+		async function injectSearchOptions() {
+			let filtermode = await Config.get("filtermode") || "blur";
+			search_results.dataset.aiwarning = filtermode;
+			const container = html`
+				<div class="block search_collapse_block" data-collapse-name="aiwarning_filter" id="aiwarning_filter" data-gpnav="rows" data-gpfocus="group">
+					<div data-panel='{"focusable":true,"clickOnActivate":true}' role="button" class="block_header labs_block_header" title="${name} v${version}">
+						<div>Narrow by ${msg.aidisclosure.english}</div>
+					</div>
+					<div class="block_content block_content_inner" style="display: block;">
+						<input type="hidden" id="hide" name="hide" value="">
+					</div>
+				</div>
+			`;
+			const content = container.querySelector(".block_content");
+			const options = [
+				{value: "blur", label: "Blur games with AI disclosure"},
+				{value: "hide", label: "Hide games with AI disclosure"}
+			];
+			options.forEach(({value, label}) => {
+				const option = html`
+					<div class="tab_filter_control_row" data-param="aiwarning_filter" data-value="${value}" data-clientside="1">
+						<span data-panel="{&quot;focusable&quot;:true,&quot;clickOnActivate&quot;:true}" role="button" class="tab_filter_control tab_filter_control_include " data-param="aiwarning_filter" data-value="${value}" data-clientside="1" data-gpfocus="item">
+							<span class="tab_filter_label_container">
+								<span class="tab_filter_control_checkbox"></span>
+								<span class="tab_filter_control_label">${label}</span>
+								<span class="tab_filter_control_count" style="display: none;"></span>
+							</span>
+						</span>
+					</div>
+				`;
+				if (value == filtermode) {
+					const checked = option.classList.add("checked");
+					option.querySelector(".tab_filter_control").classList.add("checked");
+				}
+				option.addEventListener("click", () => {
+					content.querySelectorAll(".checked").forEach((element) => {
+						if (element != option) element.classList.remove("checked");
+					});
+					const checked = option.classList.toggle("checked");
+					option.querySelector(".tab_filter_control").classList.toggle("checked", checked);
+					filtermode = checked ? value : "show";
+					search_results.dataset.aiwarning = filtermode;
+					Config.set("filtermode", filtermode);
+					findNew();
+				});
+				content.append(option);
+			});
+			const last = document.querySelector("#additional_search_options :nth-last-child(-n + 1 of .block)");
+			last.after(container);
+			const contentHeight = content.offsetHeight + "px";
+			const collapsed = await Config.get("searchoptions-collapsed") || false;
+			if (collapsed) {
+				content.style.maxHeight = 0;
+				container.classList.add("collapsed");
+			} else {
+				content.style.maxHeight = contentHeight;
+			}
+			function toggleCollapsed() {
+				content.offsetHeight; // workaround to force correct rendering
+				const collapsed = container.classList.toggle("collapsed");
+				content.style.maxHeight = collapsed ? 0 : contentHeight;
+				Config.set("searchoptions-collapsed", collapsed);
+			}
+			container.querySelector(".block_header").addEventListener("click", toggleCollapsed);
+		}
+		injectSearchOptions();
+
+		async function check(row) {
+			const appid = row.dataset.dsAppid;
+			try {
+				row.dataset.aiwarningChecked = true;
+				let aidisclosure = false;
+				const cached = await AIDisclosureCache.get(appid);
+				if (cached !== undefined) {
+					aidisclosure = cached;
+				} else {
+					row.classList.add("aiwarning_checking");
+					const url = `https://store.steampowered.com/app/${appid}?l=english`;
+					const response = await fetch(url);
+					const text = await response.text();
+					const match = text.match(`<h2>${msg.aidisclosure.english}</h2>`);
+					aidisclosure = !!match;
+					AIDisclosureCache.set(appid, aidisclosure);
+				}
+				if (aidisclosure) row.classList.add("aiwarning_hasdisclosure");
+			} catch(error) {
+				console.error(`Check for ${msg.aidisclosure.english} of App ${appid} failed.`, error);
+				row.classList.add("aiwarning_failed");
+			} finally {
+				row.classList.remove("aiwarning_checking");
+			}
+		}
+		function findNew() {
+			const rows = document.querySelectorAll("#search_results[data-aiwarning]:not([data-aiwarning=show]) #search_resultsRows > a.search_result_row[data-ds-appid]:not([data-aiwarning-checked])");
+			rows.forEach(row => check(row));
+		}
+		const observer = new MutationObserver(findNew);
+		observer.observe(search_results, {childList: true, subtree: true});
+	}
 })();
